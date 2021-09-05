895c08709d97db0b23dbfcfedeabb32b3d1a402e
Ethanhalford2008 <90108417+Ethanhalford2008@users.noreply.github.com>
2021-09-05 20:12:39 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var box 
+
+
+function setup() {
+  createCanvas(400,400);
+  box= createSprite (200,200,50,50)
+box.shapeColor="red";
+}
+
+function draw() 
+{
+  
+  
+  background(150);
+  drawSprites();
+}
+
+
+
+
